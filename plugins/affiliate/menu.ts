9ecbd0ac98a6b23f9777da95833f37ccb04c89a4
heitaoa567 <dp0219heitaoa@gmail.com>
2025-11-30 16:26:24 +0530

    Update menu.ts

--- plugins/affiliate/menu.ts
+++ plugins/affiliate/menu.ts
@@ -1,1 +1,15 @@
+// plugins/affiliate/menu.ts
+export function affiliateMenu(userId: number) {
+    const link = `https://t.me/YourMenuBot?start=${userId}`;
+    
+    return {
+        inline_keyboard: [
+            [{ text: "📣 复制推广链接", callback_data: "aff.link" }],
+            [{ text: "📊 我的推广数据", callback_data: "aff.stats" }],
+            [{ text: "🌳 我的下级结构", callback_data: "aff.tree" }],
+            [{ text: "⬅ 返回", callback_data: "menu.back" }]
+        ],
+        link
+    };
+}
 
