a72bb79622200051b76676fc5b6f433999776026
heitaoa567 <dp0219heitaoa@gmail.com>
2025-11-30 15:45:58 +0530

    Update index.ts

--- plugins/wallet/index.ts
+++ plugins/wallet/index.ts
@@ -1,1 +1,25 @@
+// ========================================
+//           Wallet 插件入口
+//    /plugins/wallet/index.ts
+// ========================================
+
+import { registerPlugin } from "../../libs/core/plugins.ts";
+import { registerMenu } from "../../libs/core/menu.ts";
+
+import { walletMenu } from "./menu.ts";
+import { walletOnMessage } from "./handler.ts";
+import { walletOnCallback } from "./callback.ts";
+
+// 注册钱包插件
+registerPlugin({
+  name: "wallet",
+  onMessage: walletOnMessage,
+  onCallback: walletOnCallback,
+  onMenu: walletMenu,
+});
+
+// 在主菜单注册按钮
+registerMenu("钱包中心💰", walletMenu);
+
+console.log("[PLUGIN] Wallet plugin loaded");
 
