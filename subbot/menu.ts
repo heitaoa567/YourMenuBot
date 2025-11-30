14bfe6257a9446f52e9c0f402ab361f1bf51ea29
heitaoa567 <dp0219heitaoa@gmail.com>
2025-11-30 16:51:52 +0530

    Update menu.ts

--- subbot/menu.ts
+++ subbot/menu.ts
@@ -1,1 +1,14 @@
+// plugins/subbot/menu.ts
+
+export function subMenu() {
+    return {
+        inline_keyboard: [
+            [{ text: "➕ 绑定子机器人", callback_data: "sub.bind" }],
+            [{ text: "🤖 我的子机器人", callback_data: "sub.list" }],
+            [{ text: "📢 子机器人广播", callback_data: "sub.broadcast" }],
+            [{ text: "🧩 九宫格菜单管理", callback_data: "sub.menus" }],
+            [{ text: "⬅ 返回", callback_data: "menu.back" }]
+        ]
+    };
+}
 
