d5705b34200453d3f39c1e4f7485263d162678e6
heitaoa567 <dp0219heitaoa@gmail.com>
2025-11-30 16:33:26 +0530

    Update menu.ts

--- plugins/supply/menu.ts
+++ plugins/supply/menu.ts
@@ -1,1 +1,13 @@
+// plugins/supply/menu.ts
+
+export function supplyMenu() {
+    return {
+        inline_keyboard: [
+            [{ text: "📝 发布供需", callback_data: "supply.new" }],
+            [{ text: "🔍 浏览供需", callback_data: "supply.browse" }],
+            [{ text: "📊 我的发布", callback_data: "supply.my" }],
+            [{ text: "⬅ 返回", callback_data: "menu.back" }]
+        ]
+    };
+}
 
