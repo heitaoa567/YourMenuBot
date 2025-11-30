0f62e533481839a4460c0ce3fc77bb6a22635c60
heitaoa567 <dp0219heitaoa@gmail.com>
2025-11-30 16:33:10 +0530

    Update types.ts

--- plugins/supply/types.ts
+++ plugins/supply/types.ts
@@ -1,1 +1,16 @@
+// plugins/supply/types.ts
+
+export interface SupplyItem {
+    id: string;
+    uid: number;
+    type: "supply" | "demand";
+    title: string;
+    content: string;
+    created: number;
+    views: number;
+    category: string;
+    contact: string;
+    ref_link: string; // 推广链接
+    vip_boost: boolean;
+}
 
