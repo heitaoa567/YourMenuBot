cd5b6653c5b96a82e22d92256d5ce64fd65f6dfb
heitaoa567 <dp0219heitaoa@gmail.com>
2025-11-30 16:51:09 +0530

    Update index.ts

--- subbot/index.ts
+++ subbot/index.ts
@@ -1,1 +1,14 @@
+// plugins/subbot/index.ts
+import type { BotPlugin } from "../../types.ts";
+import { subMenu } from "./menu.ts";
+import { handleSubCallback } from "./callback.ts";
+import { handleSubMessage } from "./handler.ts";
+
+export const subbotPlugin: BotPlugin = {
+    name: "subbot",
+    description: "子机器人系统（绑定 / 广播 / 按钮管理）",
+    menu: subMenu,
+    callbacks: handleSubCallback,
+    commands: handleSubMessage
+};
 
