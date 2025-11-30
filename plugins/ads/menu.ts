24392a9dbc79fc8ddeedec13874b862494126723
heitaoa567 <dp0219heitaoa@gmail.com>
2025-11-30 16:43:28 +0530

    Update menu.ts

--- plugins/ads/menu.ts
+++ plugins/ads/menu.ts
@@ -1,1 +1,12 @@
+// plugins/ads/menu.ts
+
+export function adsMenu() {
+    return {
+        inline_keyboard: [
+            [{ text: "📢 查看所有广告位", callback_data: "ads.list" }],
+            [{ text: "➕ 新增广告", callback_data: "ads.add" }],
+            [{ text: "⬅ 返回菜单", callback_data: "menu.back" }]
+        ]
+    };
+}
 
