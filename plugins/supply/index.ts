dd1246d00c3329a6c087eba8e9375e366c447f9d
heitaoa567 <dp0219heitaoa@gmail.com>
2025-11-30 16:32:51 +0530

    Update index.ts

--- plugins/supply/index.ts
+++ plugins/supply/index.ts
@@ -1,1 +1,14 @@
+// plugins/supply/index.ts
+import type { BotPlugin } from "../../types.ts";
+import { supplyMenu } from "./menu.ts";
+import { handleSupplyCallback } from "./callback.ts";
+import { handleSupplyText } from "./post.ts";
+
+export const supplyPlugin: BotPlugin = {
+    name: "supply",
+    description: "供需市场系统",
+    menu: supplyMenu,
+    callbacks: handleSupplyCallback,
+    commands: handleSupplyText
+};
 
