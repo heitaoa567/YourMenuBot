22e3f956ff052c760a1851955b19d05b777ecabb
heitaoa567 <dp0219heitaoa@gmail.com>
2025-11-30 16:27:03 +0530

    Update handler.ts

--- plugins/affiliate/handler.ts
+++ plugins/affiliate/handler.ts
@@ -1,1 +1,10 @@
+// plugins/affiliate/handler.ts
+import { affiliateMenu } from "./menu.ts";
+
+export async function handleAffiliateText(msg: any, reply: Function) {
+    if (!msg.text.startsWith("/aff")) return;
+
+    const chatId = msg.from.id;
+    await reply(chatId, "📣 *推广中心*", affiliateMenu(chatId));
+}
 
