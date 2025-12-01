55d0304f56043c8770c93e37fa543f3252fd9b29
heitaoa567 <dp0219heitaoa@gmail.com>
2025-12-01 11:30:11 +0530

    Delete plugins directory

--- plugins/affiliate/rules.ts
+++
@@ -1,9 +0,0 @@
-// plugins/affiliate/rules.ts
-
-export const AFFILIATE_LEVELS = [
-    { level: 1, rate: 0.40 }, // 一级 40%
-    { level: 2, rate: 0.10 }, // 二级 10%
-    { level: 3, rate: 0.05 }, // 三级 5%
-    { level: 4, rate: 0.02 }, // 四级 2%
-];
-
