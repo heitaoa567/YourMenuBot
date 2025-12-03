1656eff9b04eda0ca9f26a548e9a679071cb983f
heitaoa567 <dp0219heitaoa@gmail.com>
2025-12-03 12:49:09 +0530

    Update index.ts

--- plugins/vip/index.ts
+++ plugins/vip/index.ts
@@ -1,1 +1,15 @@
+// ======================================================================
+//                        plugins/vip/index.ts
+//                            VIP 主入口
+// ======================================================================
+
+import { vipMenu } from "./menu.ts";
+import { onCallback as vipCallback } from "./callback.ts";
+import { isVIP } from "./check.ts";
+
+export const VIP = {
+  menu: vipMenu,
+  callback: vipCallback,
+  isVIP,
+};
 
